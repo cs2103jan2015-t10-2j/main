ca9d5c8d4c4c807e7112b90b9d1f17de85e41c47
Lily Yung <ymll@live.hk>
2015-03-04 01:27:34 +0800

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+CS2103 Software Engineering
+Group t10-2j TaskHackerPro
---
+++ src/TaskHackerPro.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author t10-2j
+ *
+ */
+
+public class TaskHackerPro {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TaskHackerPro!");
+    }
+}
