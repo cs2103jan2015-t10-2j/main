1af87d158d40b98ca082d0f4d0d45f0aabb145de
Lily Yung <ymll@live.hk>
2015-03-04 12:19:56 +0800

    create a class for events

---
+++ src/Event.java
@@ -0,0 +1,76 @@
+public class Event {
+
+    private String taskName;
+    private String taskTime;
+    private String taskDate;
+    private String taskLocation;
+    private String taskDescription;
+    private TaskPriority taskPriority;
+    private boolean isDone;
+    private boolean isRecurring;
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskTime() {
+        return taskTime;
+    }
+
+    public void setTaskTime(String taskTime) {
+        this.taskTime = taskTime;
+    }
+
+    public String getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+
+    public String getTaskLocation() {
+        return taskLocation;
+    }
+
+    public void setTaskLocation(String taskLocation) {
+        this.taskLocation = taskLocation;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public TaskPriority getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTaskPriority(TaskPriority taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public boolean isRecurring() {
+        return isRecurring;
+    }
+
+    public void setRecurring(boolean isRecurring) {
+        this.isRecurring = isRecurring;
+    }
+
+}
---
+++ src/TaskPriority.java
@@ -0,0 +1,4 @@
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
