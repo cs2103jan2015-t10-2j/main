1a3e5dc9c6bad9e2d0217682e2e8a5a3be0a2c1b
Lily Yung <ymll@live.hk>
2015-03-04 12:58:30 +0800

    Create HashMap for parsing commands

--- src/TaskHackerPro.java
+++ src/TaskHackerPro.java
@@ -1,3 +1,8 @@
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * 
  * @author t10-2j
@@ -5,7 +10,30 @@
  */
 
 public class TaskHackerPro {
+
+    private Map<String, ICommandHandler> commandHandlerMap;
+
+    public TaskHackerPro() {
+        commandHandlerMap = new HashMap<String, ICommandHandler>();
+        commandHandlerMap.put("add", new AddCommandHandler());
+    }
+
+    public void parseCommand(InputStream is) {
+        Scanner scanner = new Scanner(is);
+        String command = scanner.next();
+        String inputLine = scanner.nextLine().trim();
+
+        for (String key : commandHandlerMap.keySet()) {
+            if (key.equalsIgnoreCase(command)) {
+                commandHandlerMap.get(key).parseCommand(inputLine);
+            }
+        }
+
+        scanner.close();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to TaskHackerPro!");
+        new TaskHackerPro().parseCommand(System.in);
     }
 }
