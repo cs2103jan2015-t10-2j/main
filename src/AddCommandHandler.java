62252b74df550bc05ee7d25e5bc575db77b72adc
Lily Yung <ymll@live.hk>
2015-03-04 12:57:23 +0800

    Add command handler

---
+++ src/AddCommandHandler.java
@@ -0,0 +1,14 @@
+
+public class AddCommandHandler implements ICommandHandler {
+
+    @Override
+    public boolean isValid(String command) {
+        return false;
+    }
+
+    @Override
+    public void parseCommand(String command) {
+        
+    }
+
+}
