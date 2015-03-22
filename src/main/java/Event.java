cb819caabd63dd130b3086e473aa7dd1025d9325
Anshuman Mohan <anshumanmohan@live.com>
2015-03-23 02:23:05 +0800

    Added asserts to make sure that the object in question is not null.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -17,66 +17,84 @@ public class Event implements Serializable {
     private static final String toStringFormat = "ID=%d, Name=\"%s\", Location=\"%s\", Description=\"%s\", Date=%s, Priority=%s, Done=%b, Recurring=%b";
 
     public int getTaskID() {
+    	assertObjectNotNull(this);
         return taskID;
     }
 
     public void setTaskID(int taskID) {
+    	assertObjectNotNull(this);
         this.taskID = taskID;
     }
 
     public String getTaskName() {
+    	assertObjectNotNull(this);
         return taskName;
     }
 
     public void setTaskName(String taskName) {
+    	assertObjectNotNull(this);
         this.taskName = taskName;
     }
 
     public Calendar getTaskDate() {
+    	assertObjectNotNull(this);
         return taskDate;
     }
 
     public void setTaskDate(Calendar taskDate) {
+    	assertObjectNotNull(this);
         this.taskDate = taskDate;
     }
 
     public String getTaskLocation() {
+    	assertObjectNotNull(this);
         return taskLocation;
     }
 
     public void setTaskLocation(String taskLocation) {
+    	assertObjectNotNull(this);
         this.taskLocation = taskLocation;
     }
 
     public String getTaskDescription() {
+    	assertObjectNotNull(this);
         return taskDescription;
     }
 
     public void setTaskDescription(String taskDescription) {
+    	assertObjectNotNull(this);
         this.taskDescription = taskDescription;
     }
 
     public TaskPriority getTaskPriority() {
+    	assertObjectNotNull(this);
         return taskPriority;
     }
 
     public void setTaskPriority(TaskPriority taskPriority) {
+    	assertObjectNotNull(this);
         this.taskPriority = taskPriority;
     }
 
     public boolean isDone() {
+    	assertObjectNotNull(this);
         return isDone;
     }
 
     public void setDone(boolean isDone) {
+    	assertObjectNotNull(this);
         this.isDone = isDone;
     }
 
     public boolean isRecurring() {
+    	assertObjectNotNull(this);
         return isRecurring;
     }
 
     public void setRecurring(boolean isRecurring) {
+    	assertObjectNotNull(this);
+    	assert ((this.isRecurring));
+    	assert (!(this.isRecurring));
         this.isRecurring = isRecurring;
     }
 
@@ -87,6 +105,7 @@ public class Event implements Serializable {
         }
 
         Event e = (Event) o;
+    	assertObjectNotNull(this);
         boolean isSame = (this.taskID == e.taskID);
 
         return isSame;
@@ -94,9 +113,14 @@ public class Event implements Serializable {
 
     @Override
     public String toString() {
+    	assertObjectNotNull(this);
         String timeString = getTaskDate().getTime().toString();
         return String.format(toStringFormat, getTaskID(), getTaskName(),
                 getTaskLocation(), getTaskDescription(), timeString,
                 getTaskPriority(), isDone(), isRecurring());
     }
+    
+	private void assertObjectNotNull(Object o) {
+		assert (o != null);
+	}
 }
