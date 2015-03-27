ac6f046b86762a73b6a5427673f3822ad1053a64
Anshuman Mohan <anshumanmohan@live.com>
2015-03-27 23:15:27 +0800

    Refactored exitCH

--- src/main/java/ExitCommandHandler.java
+++ src/main/java/ExitCommandHandler.java
@@ -1,7 +1,10 @@
 public class ExitCommandHandler implements ICommandHandler {
 
-    private TaskHackerPro taskHackerPro;
+	private TaskHackerPro taskHackerPro;
 
+    private static final String messageExiting = "Thank you for using TaskHackerPro!";
+	private static final String exitDelimiter = "exit";
+	
     public ExitCommandHandler(TaskHackerPro taskHackerPro) {
     	assertObjectNotNull(this);
         this.taskHackerPro = taskHackerPro;
@@ -9,7 +12,7 @@ public class ExitCommandHandler implements ICommandHandler {
 
     @Override
     public boolean parseCommand(String command) {
-        if ("exit".equals(command)) {
+        if (exitDelimiter.equals(command)) {
             return true;
         }
         return false;
@@ -19,7 +22,7 @@ public class ExitCommandHandler implements ICommandHandler {
     public boolean executeCommand() {
     	assertObjectNotNull(taskHackerPro);
         taskHackerPro.setContinue(false);
-        System.out.println("Bye!");
+        System.out.println(messageExiting);
         return true;
     }
 
