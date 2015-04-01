c375da2eaa21c36a22f23bffa852dcaa0a951321
Anshuman Mohan <anshumanmohan@live.com>
2015-04-01 21:00:06 +0800

    Removed YN confirmation from Delete. Close #77.

--- src/test/java/DeleteCommandHandlerTest.java
+++ src/test/java/DeleteCommandHandlerTest.java
@@ -7,7 +7,6 @@ public class DeleteCommandHandlerTest extends StringBasedTest {
     private TaskData taskData;
 
     private static final String commandAdd = "add Homework at 4:00 11/3/2015 for 60 mins @ Tembusu College desc \"Work on CS2103 project\"";
-    private static final String commandYes = "Y";
     private static final String commandDisplay = "display 11/3/2015";
     private static final String commandViewOption = "3";
     private static final String commandDelete = "delete 1";
@@ -33,7 +32,6 @@ public class DeleteCommandHandlerTest extends StringBasedTest {
         super.executeCommand(commandDisplay);
         super.executeCommand(commandViewOption);
         super.executeCommand(commandDelete);
-        super.executeCommand(commandYes);
         assertEquals(0, taskData.getEventMap().size());
     }
 }
