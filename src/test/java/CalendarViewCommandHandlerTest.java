d452961f8c1e7a5cee50f125eae449a9e37341ba
Lily Yung <ymll@live.hk>
2015-04-08 02:18:11 +0800

    Organise import

--- src/test/java/CalendarViewCommandHandlerTest.java
+++ src/test/java/CalendarViewCommandHandlerTest.java
@@ -1,11 +1,6 @@
-import static org.junit.Assert.assertTrue;
-
 import java.util.Calendar;
-//import java.util.List;
 
-//import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 
 public class CalendarViewCommandHandlerTest {
 
